8ee4216b953f72003f65da84addcfa31431427b7
Shinae Lee/이신애 <overflowingwithcash@gmail.com>
2023-03-03 12:57:28 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 156 ms, Memory: 116268 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n=int(input())
+arr = list(map(int,input().split()))
+d=deque()
+for i in range(n):
+    d.append((arr[i],i+1))
+
+result =[]
+
+current,index = d.popleft()
+result.append(index)
+for i in range(n-1):
+    if current>0:
+        for j in range(current-1):
+            d.append(d.popleft())
+    else:
+        for j in range(-current):
+            d.appendleft(d.pop())
+    current,index = d.popleft()
+    result.append(index)
+
+for x in result:
+    print(x,end=' ')
