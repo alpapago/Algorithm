4d7e7857623c3dbc0ab4ed2d23e2fa2ac4d8d5f9
Shinae Lee/이신애 <overflowingwithcash@gmail.com>
2023-04-26 15:24:03 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,25 +1,28 @@
 import sys
-input= sys.stdin.readline
 
-stack=[]
+N = int(sys.stdin.readline())
 
-n=int(input())
-for _ in range(n):
-    command=input().split()
-    if command[0]=='push':
+stack = []
+
+for _ in range(N):
+    command = sys.stdin.readline().split()
+    if command[0]== "push":
         stack.append(command[1])
-    elif command[0]=='pop':
-        if not len(stack):
+    elif command[0] == "pop":
+        if len(stack) == 0:
             print(-1)
         else:
-            print(stack.pop())
-    elif command[0]=='size':
+            print(stack[-1])
+            del stack[-1]
+    elif command[0] =="size":
         print(len(stack))
-    elif command[0]=='empty':
-        if not len(stack):
+    elif command[0] == "empty":
+        if len(stack) ==0 :
             print(1)
-        else: print(0)
-    else:
-        if not len(stack):
+        else:
+            print(0)
+    elif command[0]=="top":
+        if len(stack) ==0:
             print(-1)
-        else: print(stack[-1])
+        else:
+            print(stack[-1])
