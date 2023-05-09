62a35b329d3b478ef25a58a38964ff2e6d46ba30
Shinae Lee/이신애 <overflowingwithcash@gmail.com>
2023-05-09 22:35:41 +0900

    [Silver III] Title: 회전하는 큐, Time: 144 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,26 +1,29 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-n,m=map(int, input().split())
-d=deque([i for i in range(1,n+1)]) #[1,2,3,4,5,...,n]
-targets = list(map(int,input().split()))
-cnt=0
+n , m = map(int, input().split())
+q = deque([i for i in range(1,n+1)]) 
+elements = list(map(int,input().split()))
+cnt = 0
 
-for target in targets:
-    index = d.index(target)
-    #이건 돌리는 거고
-    if index<=len(d)//2:
+for element in elements:
+    index = q.index(element)
+
+    #원소가 2n개면, index = n(2n//2와 같다.) 인 요소는 앞으로 돌리나 뒤로 돌리나 상관없다.
+    #원소가 2n-1개면, index = n-1(2n-1//2와 같다.)인 요소를 기준으로 왼쪽 오른쪽 나뉜다.
+
+    if index<=len(q)//2:
         for i in range(index):
-            x=d.popleft()
-            d.append(x)
+            q.append(q.popleft())
             cnt+=1
     else:
-        for i in range(len(d)-index):
-            x=d.pop()
-            d.appendleft(x)
+        for i in range(len(q)-index):
+            q.appendleft(q.pop())
             cnt+=1
-    #이건 빼내는 거고
-    d.popleft()
+    
+    #인덱스 만큼 이동해서 해당 원소를 찾은 후,
+
+    q.popleft()
 
 print(cnt)
