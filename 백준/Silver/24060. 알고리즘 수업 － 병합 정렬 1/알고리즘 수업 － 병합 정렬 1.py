35809e496e9ccfd4708cd63ad6e0cc06028418b3
Shinae Lee/이신애 <overflowingwithcash@gmail.com>
2023-05-17 20:17:06 +0900

    [Silver IV] Title: 알고리즘 수업 - 병합 정렬 1, Time: 1248 ms, Memory: 332380 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -8,33 +8,34 @@ def mergesort(A):
     q = (len(A)+1)//2
     leftArr = mergesort(A[:q])
     rightArr = mergesort(A[q:])
-    return merge(leftArr,rightArr)
+    A = merge(leftArr, rightArr)
+    return A
 
 def merge(left,right):
     i = 0
     j = 0
-    tmp = []
+    mergeA = []
     while i<len(left) and j<len(right):
         if left[i] < right[j]:
-            tmp.append(left[i])
+            mergeA.append(left[i])
             answer.append(left[i])
             i += 1
         else:
-            tmp.append(right[j])
+            mergeA.append(right[j])
             answer.append(right[j])
             j += 1
 
     while i<len(left): #왼쪽 부분 배열이 남은 경우
-        tmp.append(left[i])
+        mergeA.append(left[i])
         answer.append(left[i])
         i+=1
 
     while j<len(right): #오른쪽 부분 배열이 남은 경우
-        tmp.append(right[j])
+        mergeA.append(right[j])
         answer.append(right[j])
         j+=1
     
-    return tmp
+    return mergeA
 
 
 n , k = map(int,input().split())
