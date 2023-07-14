570584eb0208e31721bf0e44bc90b02bdf11bef2
Shinae Lee/이신애 <overflowingwithcash@gmail.com>
2023-07-14 21:42:09 +0900

    [Silver III] Title: 회전하는 큐, Time: 144 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -2,28 +2,25 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-n , m = map(int, input().split())
-q = deque([i for i in range(1,n+1)]) 
-elements = list(map(int,input().split()))
+n,m = map(int,input().split())
+d = deque([i for i in range(1,n+1)]) #1 부터 n까지 input
+elements = list(map(int,input().split())) #숫자 인풋 받아서 배열로 만들기
 cnt = 0
 
 for element in elements:
-    index = q.index(element)
-
-    #원소가 2n개면, index = n(2n//2와 같다.) 인 요소는 앞으로 돌리나 뒤로 돌리나 상관없다.
-    #원소가 2n-1개면, index = n-1(2n-1//2와 같다.)인 요소를 기준으로 왼쪽 오른쪽 나뉜다.
-
-    if index<=len(q)//2:
+    index = d.index(element)
+   
+    #원소가 홀수든 짝수든, index <= n/2 면 왼쪽 한 칸 이동시키는게 맞다
+    #해당 element찾을때까지 자꾸 순환
+    if index <= len(d)//2:
         for i in range(index):
-            q.append(q.popleft())
+            d.append(d.popleft())
             cnt+=1
     else:
-        for i in range(len(q)-index):
-            q.appendleft(q.pop())
+        for i in range(len(d)-index):
+            d.appendleft(d.pop())
             cnt+=1
-    
-    #인덱스 만큼 이동해서 해당 원소를 찾은 후,
-
-    q.popleft()
+    #element랑 같은 원소 찾으면, 뽑자
+    d.popleft()
 
 print(cnt)
