a7f149b14fb87eeb2089d286f0c4a385a0a6f835
Shinae Lee/이신애 <overflowingwithcash@gmail.com>
2023-07-15 18:10:33 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 152 ms, Memory: 116108 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,21 +1,17 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
-n,k= map(int,input().split())
-q = deque()
+n,k = map(int, input().split())
+data = deque([i for i in range(1,n+1)])
+
 result = []
 
-for i in range(1,n+1):
-    q.append(i)
-
-while q:
-    for _ in range(k-1):
-        q.append(q.popleft())
-    result.append(q.popleft())
+while len(data)!=0:
+    for i in range(k-1):
+        data.append(data.popleft())
+    result.append(data.popleft())
 
 print('<',end='')
-for i in range(n-1):
+for i in range(len(result)-1):
     print(result[i],end=', ')
-print(result[-1],end='')
-print('>')
+
+print(result[-1],end='>')
