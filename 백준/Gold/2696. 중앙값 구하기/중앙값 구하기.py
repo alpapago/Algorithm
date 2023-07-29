d78c5de9249f0c7220328ee05f559bcff2153bb1
Shinae Lee/이신애 <overflowingwithcash@gmail.com>
2023-07-29 16:31:20 +0900

    [Gold II] Title: 중앙값 구하기, Time: 248 ms, Memory: 116936 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -2,52 +2,43 @@ import sys
 input = sys.stdin.readline
 import heapq
 
-tc = int(input())
-
-#시간 복잡도가 2000만번 이하 정도 되어야 시간안에 풀이가 됨.
-#9999개의 수열을 sort로 정렬하게되면, 9999log9999의 시간복잡도 소요
-#그리고 testcase 가 1000회 정도 이므로 1000* 9999log9999의 시간복잡도 소요 약 13억정도
-# 그래서 sort함수 쓰면 안되고, min heap, max heap을 만들어서 써야함
-
-for _ in range(tc):
+for _ in range(int(input())):
     n = int(input())
-    
-    answer=[]
-
     arr = []
 
-    min = []
-    max = []
+    result = []
 
-    #10개씩 input받기
     for _ in range(n//10+1):
         arr.extend(list(map(int,input().split())))
-
-    mid = arr[0]
-    answer.append(mid)
-
-    for i in range(1,len(arr)):
-        if mid<arr[i]:
-            heapq.heappush(min,arr[i])
-        else:
-            heapq.heappush(max,-arr[i])
-        # mid 갱신을 해야함
-        if not i%2:
-            if len(max) < len(min):
-                heapq.heappush(max,-mid)
-                mid = heapq.heappop(min)
-            elif len(max) > len(min):
-                heapq.heappush(min,mid)
-                mid = -heapq.heappop(max)
-            answer.append(mid)
     
-    num = str(len(answer))
+    # 처음 중앙값
+    mid = arr[0]
+    # 첫번째 중앙값을 답에 일단 넣음
+    result.append(mid)
 
-    finans = []
+    # 중앙값 양쪽 힙 만들기
+    left = []
+    right = []
 
-    finans.append(num)
-    for i in range(10,len(answer),10):
-        finans.append(' '.join(map(str,answer[i-10:i])))
-    finans.append(' '.join(map(str,answer[len(answer)//10*10:])))
-
-    print('\n'.join(finans))
+    for i in range(1,n):
+        if mid<arr[i]:
+            heapq.heappush(right,arr[i])
+        else:
+            # 내림차순으로 정렬해주는 max_heap에 추가
+            heapq.heappush(left,-arr[i])
+        # mid 갱신해야됨.
+        if not i%2: # 홀수번째 원소를 넣었을 때마다,
+            if len(left) < len(right):
+                heapq.heappush(left,-mid)
+                mid = heapq.heappop(right)
+            elif len(right) < len(left):
+                heapq.heappush(right,mid)
+                mid = -heapq.heappop(left)
+            result.append(mid)
+    
+    print(len(result))
+    for i in range(len(result)):
+        print(result[i], end = ' ')
+        if (i+1)%10 == 0:
+            print()
+    print()# 그 다음 케이스 줄바꿈.
