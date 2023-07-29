2d883be0497e22b6f4a58e34ccfeafc4f7f2610b
ShinAe Lee <overflowingwithcash@gmail.com>
2023-07-29 21:17:08 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 288 ms, Memory: 115516 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -2,30 +2,33 @@ import sys
 input = sys.stdin.readline
 
 def find(parent,x):
-    if parent[x]!=x:
-        parent[x]=find(parent,parent[x])
+    if x != parent[x]:
+        parent[x] = find(parent, parent[x])
+        
     return parent[x]
-    
-def union(parent,a,b):
-    a=find(parent,a)
-    b=find(parent,b)
-    if a<b:
-        parent[b]=a
+
+
+def union(parent,x,y):
+    x = find(parent,x)
+    y = find(parent,y)
+
+    if x<y:
+        parent[y] = x
     else:
-        parent[a]=b
+        parent[x] = y
 
-n,m=map(int,input().split())
-parent=[0]*(n+1)
 
-for i in range(1,n+1):
-    parent[i]=i
+n, m = map(int,input().split())
+parent = [i for i in range(n+1)]
 
-for i in range(m):
-    a,b=map(int,input().split())
+answer = 1
+
+for _ in range(m):
+    a, b = map(int, input().split())
     union(parent,a,b)
 
-counter=set()
+cnt = set()
 for i in range(1,n+1):
-    counter.add(find(parent,i))
+    cnt.add(find(parent,i))
 
-print(len(counter))
+print(len(cnt))
